c6599dfd5b66008c12ecfc8b8660287510b672af
Farukozdeemirrr <136840618+Farukozdeemirrr@users.noreply.github.com>
2025-04-09 12:15:47 +0300

    feat: Add UserRole enum and role property to User entity

---
+++ Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+    }
+}
